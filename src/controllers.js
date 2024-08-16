b51773097e8215353b683d65915f84dfab26f7c4
Gaston Espinola <gastonespinola146@gmail.com>
2024-08-16 14:09:54 -0300

    Se cambiaron los send por json

--- src/controllers.js
+++ src/controllers.js
@@ -2,7 +2,7 @@ import { conectar } from "./database.js"
 
 export const mostrarTareas = async(req, res) =>{
     const conexion = await conectar();
-    const [consulta] = await conexion.query("SELECT * FROM TASKS")
+    const [consulta] = await conexion.query("SELECT * FROM tasks")
     if(consulta.length==0){
         res.send("No hay tareas disponibles")
     }else{
@@ -14,21 +14,21 @@ export const agregarTarea = async(req, res) =>{
     const conexion = await conectar();
     const { title, description, isComplete } = req.body;
     if(typeof(title)!="string" || typeof(description)!="string" || typeof(isComplete)!="boolean"){
-        res.send("ERROR Algun dato no cumple con los requisitos")
+        res.json("ERROR Algun dato no cumple con los requisitos")
     }else{
         const [consulta] = await conexion.query(`INSERT INTO tasks(title, description, isComplete) VALUES('${title}','${description}', ${isComplete})`);
-        res.send("Se agrego una tarea con exito");
+        res.json("Se agrego una tarea con exito");
     }
 }
 
 export const mostrarTarea = async(req, res) =>{
     const conexion = await conectar();
     const id = parseInt(req.params.id);
-    const [consulta] = await conexion.query(`SELECT * FROM TASKS WHERE id = ${id}`)
+    const [consulta] = await conexion.query(`SELECT * FROM tasks WHERE id = ${id}`)
     if(consulta.length==0){
-        res.send("El id puesto no existe")
+        res.json("El id puesto no existe")
     }else{
-        res.send(consulta);
+        res.json(consulta);
     }
 }
 
@@ -36,23 +36,23 @@ export const cambiarTarea = async(req, res) =>{
     const conexion = await conectar();
     const id = parseInt(req.params.id);
     const {title, description, isComplete} = req.body;
-    const [consulta] = await conexion.query(`SELECT * FROM TASKS WHERE id = ${id}`);
+    const [consulta] = await conexion.query(`SELECT * FROM tasks WHERE id = ${id}`);
     if(typeof(title)!="string" || typeof(description)!="string" || typeof(isComplete)!="boolean" || consulta.length==[]){
-        res.send("ERROR Algun dato no cumple con los requisitos o el id puesto no existe")
+        res.json("ERROR Algun dato no cumple con los requisitos o el id puesto no existe")
     }else{
         const [consultaCambiar] = await conexion.query(`UPDATE tasks SET title = '${title}', description='${description}', isComplete=${isComplete} WHERE id = ${id}`)
-        res.send("Tarea editada con exito");
+        res.json("Tarea editada con exito");
     }
 }
 
 export const borrarTarea = async(req, res) =>{
     const conexion = await conectar();
     const id = parseInt(req.params.id);
-    const [consulta] = await conexion.query(`SELECT * FROM TASKS WHERE id = ${id}`);
+    const [consulta] = await conexion.query(`SELECT * FROM tasks WHERE id = ${id}`);
     if(consulta.length==[]){
-        res.send("El id puesto para borrar no existe")
+        res.json("El id puesto para borrar no existe")
     }else{
-    const [consultaBorrar] = await conexion.query(`DELETE FROM TASKS WHERE id = ${id}`)
-    res.send("La tarea se borro con exito");
+    const [consultaBorrar] = await conexion.query(`DELETE FROM tasks WHERE id = ${id}`)
+    res.json("La tarea se borro con exito");
     }
 }
